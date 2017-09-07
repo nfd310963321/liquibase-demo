8907a2c3d3f103679c158e5d97c889de4eda5031
HuangXin <huangxin@nongfadai.com>
2017-09-07 13:53:42 +0800

    do changes

--- liquibase-model/db/script/v0.0.0.1.sql
+++ liquibase-model/db/script/v0.0.0.1.sql
@@ -9,3 +9,14 @@ CREATE TABLE sms_send_log (
 );
 --rollback DROP table sms_send_info;
 --rollback DROP TABLE sms_send_log;
+
+--changeset huangxin:v0.0.0.1-2
+CREATE table sms_send_info1(
+id int primary key auto_increment
+);
+CREATE TABLE sms_send_log1 (
+  ID int primary key auto_increment,
+  NAME VARCHAR(255) NULL
+);
+--rollback DROP table sms_send_info1;
+--rollback DROP TABLE sms_send_log1;
